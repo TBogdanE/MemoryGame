b0de8d61867f9ddd9d97b8b823b0be90076e0baa
Toma Bogdan <bogdantoma2000@gmail.com>
2024-04-08 16:36:37 +0300

    update - move shufflearray function in sepparate file

--- MemoryGame/src/components/GameBoard.jsx
+++ MemoryGame/src/components/GameBoard.jsx
@@ -12,14 +12,6 @@ export default function GameBoard({ difficulty }) {
     setCards(selectedCards);
   }, [difficulty]);
 
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
   return (
     <div id="gameboard">
       {cards.map((card) => (
